2d8d3963e1cc03cd54747ed8831b778289672584
OhioLight <142236532+OhioLight@users.noreply.github.com>
2023-12-06 15:31:32 +0100

    4.5.2_abgeschlossen

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
@@ -4,6 +4,52 @@ class S4524 extends StatelessWidget {
   const S4524({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const ShowHideNameWidget();
+  }
+}
+
+class ShowHideNameWidget extends StatefulWidget {
+  const ShowHideNameWidget({super.key, this.nameText});
+
+  final String? nameText;
+
+  @override
+  State<ShowHideNameWidget> createState() => _ShowHideNameWidgetState();
+}
+
+class _ShowHideNameWidgetState extends State<ShowHideNameWidget> {
+  String _name = '';
+  bool _isVisible = false;
+
+  void _toggleVisibility() {
+    setState(() {
+      _isVisible = !_isVisible;
+      if (_isVisible) {
+        _name = 'Nico Rudloff';
+      } else {
+        _name = '';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            _name,
+            style: const TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: _toggleVisibility,
+            child: Text(_isVisible ? 'Name verstecken' : 'Name anzeigen'),
+          ),
+        ],
+      ),
+    );
   }
 }
