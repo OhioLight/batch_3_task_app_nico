56b9cd028e1c69a8b2baeb9728d7d1583be75203
OhioLight <142236532+OhioLight@users.noreply.github.com>
2024-01-02 12:06:32 +0100

    5111 und5112 fertig

--- lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_1.dart
+++ lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_1.dart
@@ -4,6 +4,64 @@ class S5111 extends StatelessWidget {
   const S5111({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const Aufgabe1();
+  }
+}
+
+class Aufgabe1 extends StatefulWidget {
+  const Aufgabe1({super.key});
+
+  @override
+  State<Aufgabe1> createState() => _Aufgabe1State();
+}
+
+bool isSwitched = false;
+
+class _Aufgabe1State extends State<Aufgabe1> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          const Text(
+            'Hello World!',
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+              ),
+              const SizedBox(
+                width: 50,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Switch(
+            activeColor: Colors.amber.shade800,
+            value: isSwitched,
+            onChanged: (value) {
+              setState(() {
+                isSwitched = value;
+              });
+            },
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_2/s_5_1_1_2.dart
+++ lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_2/s_5_1_1_2.dart
@@ -4,6 +4,79 @@ class S5112 extends StatelessWidget {
   const S5112({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const Aufgabe5112();
+  }
+}
+
+class Aufgabe5112 extends StatelessWidget {
+  const Aufgabe5112({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var upperStyle = const TextStyle(
+        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black);
+    var hintStyle = const TextStyle(fontSize: 16, color: Colors.black);
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Container(
+        color: Colors.white,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              children: [
+                const Image(
+                  image: AssetImage('assets/images/solutions/lama.png'),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  'Name',
+                  style: upperStyle,
+                ),
+                Text(
+                  'Diana das Lama',
+                  style: hintStyle,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  'Gewicht',
+                  style: upperStyle,
+                ),
+                Text(
+                  '120 kg',
+                  style: hintStyle,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  'Größe',
+                  style: upperStyle,
+                ),
+                Text(
+                  '1,80 m',
+                  style: hintStyle,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  'Lieblingsessen',
+                  style: upperStyle,
+                ),
+                Text(
+                  'Gras',
+                  style: hintStyle,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
